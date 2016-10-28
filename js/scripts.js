877353de1dde062c92bc852a9a81f3d4e9979daa
Anne Belka <antastic461@gmail.com>
2016-10-28 09:22:11 -0700

    add ability to detect if provided number is divisible by 5 and output pong

--- js/scripts.js
+++ js/scripts.js
@@ -7,9 +7,15 @@ for(var i=1; i<=inputNumber; i++) {
 counts;
 
 for(var i=0;i<=inputNumber;i++) {
-  if (counts[i] % 3 === 0)
+  if (counts[i] % 3 === 0){
   counts.push("ping");
-}
+  } else if (counts[i] % 5 === 0){
+      counts.push("pong");
+    }
+  }
+
+  counts;
+
 
 
 
